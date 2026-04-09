75877d649aebedacd2300593d88056de4842226f
ZeindraGamers <zeindraraja04@gmail.com>
2026-04-10 02:52:18 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   port: process.env.DB_PORT
-  database: databaseName,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
 });
